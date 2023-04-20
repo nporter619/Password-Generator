76e5911feff9e8628d0cd2d3e1d410f9c5f8d3fb
nporter <nporterwork@gmail.com>
2023-04-20 12:04:21 -0700

    added constants, let variables to hold input and the get random function

--- develop/script.js
+++ develop/script.js
@@ -1,14 +1,26 @@
 // Assignment Code
+// List of acceptable special characters
+const specialChars = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+// List of acceptable uppercase and lowercase characters
+const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercaseChars = uppercaseChars.toLowerCase();
+
+// Variable to hold the users input
+let passwordLength = 0;
+let containsLowercase = false;
+let containsUppercase = false;
+let containsNumbers = false;
+let containsSpecial = false;
+
+// Reference the generate password button
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+// Function used to generate a random number between 0 and the specified number-1
+function getRandom(num) {
+    return Math.floor(Math.random() * num);
 }
+
+function writePassword()
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
